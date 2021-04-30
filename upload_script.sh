32e7dcbb48f84fcf940816a9d9e6ef21e2fbdcd1
Sebastian James <sebaxj@stanford.edu>
2021-04-30 04:39:29 -0700

    finish test 2

--- upload_script.sh
+++ upload_script.sh
@@ -20,7 +20,7 @@ git branch
 
 # run ffmpeg-normalize
 mkdir normalized
-for FILE in *.wav; do ffmpeg-normalize $FILE -ofmt wav; done
+for FILE in *.wav; do ffmpeg-normalize $FILE -o $FILE.wav; done
 
 # convert markdown to html for webpage
 pandoc -s index.md -o index.html
