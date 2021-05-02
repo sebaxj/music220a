479693de37a6c22f32a2be8916a1e3101f12c0e5
Sebastian James <sebaxj@stanford.edu>
2021-05-01 18:00:31 -0700

    del nor.sh

--- normalize.sh
+++
@@ -1,5 +0,0 @@
-echo A simple branch, pull, ffmpeg-normalize, .md to .html, push script!
-read -p "What is the directory for this HW? " hw_name
-cd $hw_name/wav
-
-for FILE in *.wav; do ffmpeg-normalize $FILE -ext wav -of /../normalized/; done
